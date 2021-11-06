23b592b790a42f7e6dba8ec4724e8e978243f96a
Michael Peres <52138450+makiisthenes@users.noreply.github.com>
2021-11-06 20:10:07 +0000

    Delete src directory

--- src/IssueReport.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function IssueReport() {
-	return (
-		<div>
-			<h2> This is a placeholder element for the issue report component.</h2>
-		</div>
-	)
-}
